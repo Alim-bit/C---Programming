aa76b89423e118d4cc6e5319988b4c160bbebaf5
Alim Dhuka <alimdhuka@Alims-MacBook-Air.local>
2024-01-27 16:12:21 -0500

    adding gcd.c

---
+++ gcd.c
@@ -0,0 +1,30 @@
+#include <math.h> 
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+
+// gcd(int m, int n) returns the greatest common denominator of m and n.
+// requires: m, n must be positive
+int gcd(int m, int n) {
+  // your implementation goes here
+  assert(m >= 0);
+  assert(n >= 0);
+  if (n == 0) {
+        return m;
+  } else {
+    return gcd(n, m % n);
+  }
+  return 0;
+}
+
+int main(void) {
+  // public tests
+  assert(gcd(18, 27) == 9);
+
+  // your additional tests go here
+  assert(gcd(2, 3) == 1);
+  assert(gcd(25, 10) == 5);
+  assert(gcd(23, 25) == 1);
+  trace_msg("All assertion-based tests passed!");
+  return EXIT_SUCCESS;
+}
